4f0dc419daf92208ffcdc0b2dbb06f91a11af5ab
Sascha Bürk <macfly@german-bash.org>
2021-09-30 02:03:27 +0200

    Added option for path to input file

--- README.md
+++ README.md
@@ -9,13 +9,16 @@ This script is used for converting a `disc.info` generated from `Wii Backup Mana
 ## Usage
 
 ```text
-usage: discinfo2titles.py [-h] [-V] [-v] [-p]
+usage: discinfo2titles.py [-h] [-V] [-v] [-d] [file]
 
 Converts a disc.info to titles.txt
 
+positional arguments:
+  file           the full path to disc.info (default: disc.info)
+
 optional arguments:
-  -h, --help      show this help message and exit
-  -V, --version   show program's version number and exit
-  -v, --verbose   More Output
-  -p, --preserve  Don't delete disc.info
+  -h, --help     show this help message and exit
+  -V, --version  show program's version number and exit
+  -v, --verbose  More Output
+  -d, --delete   delete disc.info after converting
   ```
--- discinfo2titles.py
+++ discinfo2titles.py
@@ -4,7 +4,7 @@ File: discinfo2titles.py
 Created Date: 29.09.2021 23:23:31
 Author: Sascha Buerk
 Email: macfly@german-bash.org
-Last Modified: 30.09.2021 01:07:46
+Last Modified: 30.09.2021 01:59:51
 """
 
 __author__ = "Sascha Buerk"
@@ -14,52 +14,66 @@ __version__ = 1.0
 import configparser
 import argparse
 import os
+from shutil import copyfile
 
+# Variables
+inputfile = "disc.info"
+output = "titles.txt"
 
-### Arguments
-parser = argparse.ArgumentParser(description='Converts a disc.info to titles.txt')
-parser.add_argument('-V', '--version', action='version', version='%(prog)s {v}'.format(v=__version__))
+# Arguments
+parser = argparse.ArgumentParser(
+    description='Converts a disc.info to titles.txt')
+parser.add_argument('file', metavar='file', nargs='?',
+                    default=inputfile, help='the full path to disc.info (default: %(default)s)')
+parser.add_argument('-V', '--version', action='version',
+                    version='%(prog)s {v}'.format(v=__version__))
 parser.add_argument('-v', '--verbose', action='store_true', help='More Output')
-parser.add_argument('-p', '--preserve', action='store_false', help='Don\'t delete disc.info')
+parser.add_argument('-d', '--delete', action='store_true',
+                    help='delete disc.info after converting')
 args = parser.parse_args()
 
+inputpath = args.file
+tmppath = "/tmp/" + inputfile
+input = tmppath
+
 try:
-	### Variables
-	input = "disc.info"
-	output= "titles.txt"
+    copyfile(inputpath, input) # copy input to tmp
 
-	### Prepare disc.info
-	a_file = open(input, "r")
-	lines = a_file.readlines()
-	a_file.close()
+    # Prepare disc.info
+    a_file = open(input, "r")
+    lines = a_file.readlines()
+    a_file.close()
 
-	if (lines[0] == "\ufeffWBM disc info\n"):
-		del lines[:2] # Delete the first two lines
+    if (lines[0] == "\ufeffWBM disc info\n"):
+        del lines[:2]  # Delete the first two lines
 
-		new_file = open(input, "w+") # Write back to outputfile
-		for line in lines:
-			new_file.write(line)
-		new_file.close()
+        new_file = open(input, "w+")  # Write back to outputfile
+        for line in lines:
+            new_file.write(line)
+        new_file.close()
 
-	open(output, 'w').close() # clear current or make new output file
-	f = open(output, "a") # Open output for append
+    open(output, 'w').close()  # clear current or make new output file
+    f = open(output, "a")  # Open output for append
 
-	config = configparser.ConfigParser()
-	config.read(input)
+    config = configparser.ConfigParser()
+    config.read(input)
 
-	for key in config.sections():
-		f.write(key + " = " + config[key]['Original Title'] + "\n")
-		if args.verbose: (print(key + " = " + config[key]['Original Title']))
-	f.close()
+    for key in config.sections():
+        f.write(key + " = " + config[key]['Original Title'] + "\n")
+        if args.verbose:
+            (print(key + " = " + config[key]['Original Title']))
+    f.close()
 
-	if args.preserve: os.remove(input)
+    if args.delete:
+        os.remove(args.file)
 
-	print("All done, output written to {output}".format(output=output))
-	exit(0)
+    print("All done, output written to {output}".format(output=output))
+    exit(0)
 
+### Error Handling
 except FileNotFoundError:
-	print("{input} not found.".format(input=input))
-	exit(1)
+    print("{input} not found.".format(input=inputfile))
+    exit(1)
 except PermissionError:
-	print("{output} not writeable.".format(output=output))
-	exit(2)
+    print("{output} not writeable.".format(output=output))
+    exit(2)
