9d7421c2b21a46a32a80cee32fbc99dc64c82250
Sascha Bürk <macfly@german-bash.org>
2021-09-30 05:24:53 +0200

    added license text

--- discinfo2titles.py
+++ discinfo2titles.py
@@ -5,12 +5,22 @@ Created Date: 29.09.2021 23:23:31
 Author: Sascha Buerk
 Email: macfly@german-bash.org
 License: GPL-3.0-only
-Last Modified: 30.09.2021 04:53:10
+Last Modified: 30.09.2021 05:24:11
+
+Copyright (C) 2021 Sascha Buerk
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, version 3. This program is distributed in the
+hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details. You should have 
+received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
 """
 
 __author__ = "Sascha Buerk"
 __email__ = "macfly@german-bash.org"
-__license__ = "GPLv3"
+__license__ = "GPLv3-only"
 __version__ = 1.0
 
 import configparser
