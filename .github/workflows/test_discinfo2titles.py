635323b02522adb085a5f8f02b1485c75e8f64e3
Sascha Bürk <macfly@german-bash.org>
2023-04-27 04:45:42 +0200

    test testing

---
+++ .github/workflows/test_discinfo2titles.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Filename: test_discinfo2titles.py
+Created Date: 27.04.2023 04:44:44
+Author: Sascha Buerk
+Email: macfly@german-bash.org
+License: GPL-3.0-or-later
+Last Modified: 27.04.2023 04:45:20
+
+Copyright (C) 2023 Sascha Bürk <macfly@german-bash.org>
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will beuseful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+__author__ = "Sascha Buerk"
+__email__ = "macfly@german-bash.org"
+__license__ = "GPL-3.0-or-later"
+__version__ = 0.0
+
+
+def test_greeting():
+    print('Hello, world!')
+
+
+def test_checkout():
+    print("This is checkout")
